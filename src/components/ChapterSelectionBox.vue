8553724c7e26d9348776d795d0822768a1d1fc00
Taufeq Mohammed <taufeq_star@Taufeqs-iMac.local>
2018-09-01 01:00:50 +0530

    Implemented interactive squad formation feature. Need to communicate with stores next

---
+++ src/components/ChapterSelectionBox.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input type="checkbox" id="chapter-option" 
+        @select="$emit('toggleSelection', $event.target.value)">
+        <label for="chapter-option">{{ value.name }}</label>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        value: {
+            type: Object,
+            required: true,
+        }
+    },
+})
+</script>
+
--- src/components/SquadBox.vue
+++ src/components/SquadBox.vue
@@ -1,16 +1,32 @@
 <template>
 	<div>
-        {{ squad.name }}
+        {{ squad.name }} <br>
+        <chapter-selection-box v-for="chapter in chapters" :key="chapter.id" 
+        :value="chapter" @toggleSelection="putChapter"></chapter-selection-box>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import ChapterSelectionBox from './ChapterSelectionBox.vue';
 export default Vue.extend({
     props: {
         squad: {
             type: Object,
             required: true,
+        }
+    },
+    data() {
+        return {
+            chapters: this.$store.state.chapters,
+        }
+    },
+    components: {
+        ChapterSelectionBox
+    },
+    methods: {
+        putChapter: function(event: boolean) {
+
         }
     }
 })
